753d4c3cbeebb5842f664e264789e7c58f0aea5c
praveen2896 <mpraveenkumarown@gmail.com>
2016-07-22 14:51:40 +0530

    Create Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,9 @@
+# Reverse
+public class Reverse {
+public static void main(String[] args) {
+	String s="codekata";
+	StringBuffer str=new StringBuffer(s);
+    String ans=	str.reverse()+"";
+	System.out.println(ans);
+}
+}
