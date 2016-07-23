ae0104b97ebfc8fd128dbf56859ed66dc00cabd5
praveen2896 <mpraveenkumarown@gmail.com>
2016-07-23 10:22:46 +0530

    Add files via upload

---
+++ String_To_Integer.java
@@ -0,0 +1,12 @@
+package String;
+
+public class String_To_Integer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       String s="praveen";
+       Integer n=Integer.valueOf("12345");
+       System.out.println(n);
+	}
+
+}
