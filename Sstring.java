697ee75cb1f8f5d4ae8436b54d73060ccacfddb4
praveen2896 <mpraveenkumarown@gmail.com>
2016-07-23 12:31:33 +0530

    Add files via upload

---
+++ Sstring.java
@@ -0,0 +1,19 @@
+package String;
+
+public class Sstring {
+ public static void main(String[] args) {
+	String s="codekata";
+	if(s.contains("a"))
+	{
+		System.out.println("it contains a");
+	}
+	if(s.contains("ta"))
+	{
+		System.out.println("it contains ta");
+	}
+	else
+	{
+		System.out.println("it does not contains your option");
+	}
+}
+}
