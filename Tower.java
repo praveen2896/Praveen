0d407fa3d0d2722649b667dae4a79beea694fd5b
praveen2896 <mpraveenkumarown@gmail.com>
2016-08-08 14:45:46 +0530

    Add files via upload

---
+++ Tower.java
@@ -0,0 +1,26 @@
+package String;
+
+import java.util.Scanner;
+
+public class Tower {
+	
+
+		   public void Answer(int n, String start, String auxiliary, String end) {
+		       if (n == 1) {
+		           System.out.println(start + " -> " + end);
+		       } else {
+		           Answer(n - 1, start, end, auxiliary);
+		           System.out.println(start + " -> " + end);
+		           Answer(n - 1, auxiliary, start, end);
+		       }
+		   }
+
+		   public static void main(String[] args) {
+		       Tower towers = new Tower();
+		       System.out.print("Enter number of discs: ");
+		       Scanner s = new Scanner(System.in);
+		       int discs = s.nextInt();
+		       towers.Answer(discs, "A", "B", "C");
+		   }
+		}
+
